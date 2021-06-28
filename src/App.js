e28738f5a4c9b7c8ad8aa9ab18f117defb8bec05
himanshu8174HC <himanshuchaudhary8174@gmail.com>
2021-06-28 09:36:02 +0530

    changing path of forms

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from "react";
 import Home from "./Containers/Home";
-import  SignUpForm from "./Components/SignUpForm";
+import  SignUpForm from "./Components/form/SignUpForm";
 import GenralPro from "./Components/GenralPro";
 import AcademicPro from "./Components/AcademicPro";
 import CodingPro from "./Components/CodingPro";
 import {Switch, Route, Redirect} from "react-router-dom";
-import Login from "./Components/Login"
+import Login from "./Components/form/Login"
+// import {TrasitionGroup, CSSTransition} from "react-transition-group"
 
 
 function App() {
   return (
     <div>
     <Switch>
-      <Route exact path = "/" component = {Home} />
-      <Route exact path = "/signUp" component = {SignUpForm} />
-      <Route exact path = "/Login" component = {Login} />
-      <Route exact path = "/genralPro" component = {GenralPro} />
-      <Route exact path = "/academicPro" component = {AcademicPro} />
-      <Route exact path = "/codingPro" component = {CodingPro} />
-      <Redirect to = "/" />
+    
+     
+        <Route exact path = "/" component = {Home} />
+        <Route exact path = "/signUp" component = {SignUpForm} />
+        <Route exact path = "/Login" component = {Login} />
+        <Route exact path = "/genralPro" component = {GenralPro} />
+        <Route exact path = "/academicPro" component = {AcademicPro} />
+        <Route exact path = "/codingPro" component = {CodingPro} />
+        <Redirect to = "/" />
+     
     </Switch>
     </div>
   );
